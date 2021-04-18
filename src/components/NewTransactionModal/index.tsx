9fc9274703eeb02b244d46cbad20c96ba03fc4ef
Pedro Bittencourt <pedrodmbc@gmail.com>
2021-04-18 11:28:21 -0300

    Add Modal content & styles using polished

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from "react-modal";
+import { Container, TransactionsTypeContainer } from "./styles";
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +14,36 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        className="react-modal-close"
+        type="button"
+        onClick={onRequestClose}
+      >
+        <img src={closeImg} alt="fechar modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <TransactionsTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Entrada" />
+            <span>Saída</span>
+          </button>
+        </TransactionsTypeContainer>
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
