53ed1a1bafb1bff1916685c25ad29facf2d198b4
Pedro Bittencourt <pedrodmbc@gmail.com>
2021-04-18 10:12:22 -0300

    Config MirageJS & Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
+import { api } from "../../services/api";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api.get("transactions").then(res => console.log(res.data));
+  });
   return (
     <Container>
       <table>
